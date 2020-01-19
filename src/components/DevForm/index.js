8a9ba6c28520d417a058dd31ba1a2972e99982cc
Daniel <daniel_sousa.p@outlook.com>
2020-01-18 23:18:25 -0300

    Arrumando DevForm

--- src/components/DevForm/index.js
+++ src/components/DevForm/index.js
@@ -1,7 +1,5 @@
 import React, { useState, useEffect } from 'react';
 
-import './styles.css';
-
 function DevForm({ onSubmit }) {
     const [github_username, setGithubUsername] = useState('');
     const [techs, setTechs] = useState('');
